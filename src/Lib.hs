f65ad184ca562dd07bcd3f68f36494d5dde60efb
Yasuaki Kudo <yasu@yasuaki.com>
2019-06-18 07:49:23 +0900

    i

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,42 @@
 module Lib
-    ( someFunc
+  ( report
+  ) where
 
-    ) where
+import           Data.List
+import           Numeric.Natural
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+ttt = foldr (+) 0 [1 ..]
 
-data Turn = Cash Integer deriving Show
+report =
+  intercalate
+    "\n"
+    [ (show ttt)
+    , (show turns)
+    , (show $ cashsum turns)
+    , (show $ turns2cashflows turns)
+    ]
+
+turns = [Noop, Contract, Days 0]
+
+cashsum turns = sum [x | Cash x <- turns]
+
+abc Contract (Accum days cashflow) = Accum 0 cashflow
+abc turn (Accum days cashflow)     = Accum 0 cashflow
+
+turns2cashflows turns = foldr abc (Accum 0 (Cashflow [])) turns
+
+data Turn
+  = Cash Integer
+  | Contract
+  | Days Natural
+  | Noop
+  deriving (Show, Eq)
+
+data Accum =
+  Accum Natural
+        Cashflow
+  deriving (Show)
+
+data Cashflow =
+  Cashflow [Integer]
+  deriving (Show)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,4 @@
 import Lib
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =  putStrLn report
