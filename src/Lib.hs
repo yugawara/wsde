09e3f21fc51446ff2626c569f2982a87255dcecd
Yasuaki Kudo <yasu@yasuaki.com>
2019-06-21 07:51:19 +0900

    contracts, turns and days

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,29 +5,37 @@ module Lib
 import           Data.List
 import           Numeric.Natural
 
-ttt = foldr (+) 0 [1 ..]
-
 report =
   intercalate
     "\n"
-    [ (show ttt)
-    , (show turns)
-    , (show $ cashsum turns)
-    , (show $ turns2cashflows turns)
-    ]
+    [(show turns), (show $ cashsum turns), (show $ turns2cashflows turns)]
 
-turns = [Noop, Contract, Days 0]
+--turns = [Noop, Contract 2, Days 10, Contract 1]
+turns = [Days 6, Contract 2]
 
 cashsum turns = sum [x | Cash x <- turns]
 
-abc Contract (Accum days cashflow) = Accum 0 cashflow
-abc turn (Accum days cashflow)     = Accum 0 cashflow
+abc (Accum days (Cashflow orig_cashflow_values)) (Contract contract_days) =
+  Accum days zz
+  where
+    zz = Cashflow $ (map ff yy)
+    ff (x, y) = x + y
+    yy :: [(Integer, Integer)]
+    yy =
+      zip
+        ((take (fromEnum days) $ repeat 0) ++
+        --((take (fromIntegral days) $ repeat 0) ++
+         (map (\x -> (toInteger x)) [1 .. contract_days]))
+        (orig_cashflow_values ++ (repeat 0))
+abc (Accum days cashflow) (Days current_days)  =
+  Accum (current_days + days) cashflow
+abc (Accum days cashflow) turn = Accum days cashflow
 
-turns2cashflows turns = foldr abc (Accum 0 (Cashflow [])) turns
+turns2cashflows turns = foldl abc (Accum 0 (Cashflow [])) turns
 
 data Turn
   = Cash Integer
-  | Contract
+  | Contract Integer
   | Days Natural
   | Noop
   deriving (Show, Eq)
